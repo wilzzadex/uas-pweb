ac7946e1f6ca8ca0caa00f860c9f8f5a910567d1
wilzzadex <ahmadwaliyudin2018@gmail.com>
2025-02-11 18:43:59 +0700

    update

--- config/koneksi.php
+++ config/koneksi.php
@@ -1,8 +1,8 @@
 <?php
 // $host = "host.docker.internal"; // Hostname
-$host = "194.233.89.11"; // Hostname
+$host = "localhost"; // Hostname
 $username = "root"; // Username
-$password = "bismillah123"; // Password (Isi jika menggunakan password)
+$password = "root"; // Password (Isi jika menggunakan password)
 $database = "rasabersama"; // Nama databasenya
 
 $conn = mysqli_connect($host, $username, $password, $database);
